ec85df49a8836a9e019a8262d0945f48d4267314
Alexandre Sebrão <xandizitxu@iMac-de-Alexandre.local>
2016-06-16 10:56:44 -0300

    Acabou

--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from manager.views.forum import forum
 from manager.views.forum import page
+from manager.views.about import AboutView
 from . import views
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
         name='forum'),
     url(r'^forum/(?P<forum_id>[0-9]+)/(?P<page_id>[0-9]+)$', page,
         name='page'),
+    url(r'^about/', AboutView.as_view()),
 ]
