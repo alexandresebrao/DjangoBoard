b206129d8449f8bcd275cc950e0e224ede3a5118
Alexandre Sebrão <xandizitxu@iMac-de-Alexandre.local>
2016-06-15 22:19:10 -0300

    Fix WhiteNoise

--- forum/settings.py
+++ forum/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -85,7 +85,7 @@ DATABASES = {
 }
 
 # Update database configuration with $DATABASE_URL.
-import dj_database_url
+
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
@@ -128,3 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR+'/static/'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
